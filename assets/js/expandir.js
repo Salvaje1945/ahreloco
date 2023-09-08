712ddb3c31e80aa5c87a9b39d9224ca41c19ac94
nicolas <escriba.salvaje@gmail.com>
2023-09-07 23:47:51 -0300

    Mejoras en el sistema de expandir contraer de -nuestra empresa-

--- assets/js/expandir.js
+++ assets/js/expandir.js
@@ -12,8 +12,12 @@ export default function expYcontrTxt() {
     function contraerNosotros() {
         $('#nuestra-empresa_txt').classList.remove('activo')
         $('#info-nosotros-fx').classList.remove('no')
-        $('#nosotros-leer-mas').classList.remove('no')
+        // $('#nosotros-leer-mas').classList.remove('no')
         $('#nosotros-leer-menos').classList.add('no')
+
+        setTimeout(()=>{
+            $('#nosotros-leer-mas').classList.remove('no')
+        }, 300)
     }
 
     $('#nosotros-leer-mas').onclick = expandirNosotros
